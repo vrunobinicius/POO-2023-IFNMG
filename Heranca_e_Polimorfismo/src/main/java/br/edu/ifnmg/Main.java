b32c9f4456988c4a478f8f5a6a3004b6b5b5640b
Bruno Vinícius <brunoviniciusmito@gmail.com>
2023-03-31 10:49:37 -0300

    Main

---
+++ Heranca_e_Polimorfismo/src/main/java/br/edu/ifnmg/Main.java
@@ -0,0 +1,10 @@
+package br.edu.ifnmg;
+
+/**
+ * @author Bruno Vinícius
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
